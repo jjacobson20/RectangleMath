ea8efce6f6545381dfd004ad072963dce157f9fa
jjacobson20 <jjacobson20@HS-MATH2-09.gvsd.org>
2018-10-02 13:37:26 -0400

    initial commit

---
+++ src/RectangleMath.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ * @author jeremy
+ *
+ */
+import java.util.Scanner;
+
+public class RectangleMath
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the width: ");
+		
+		double width = in.nextDouble();
+		in.nextLine();
+		
+		System.out.print("Enter the height: ");
+		
+		double height = in.nextDouble();
+		in.nextLine();
+		
+		System.out.printf(%(width * height), int);
+		System.out.printf("Perimeter: " + (width * 2 + height * 2), );
+		
+		
+		
+	}
+	
+}
